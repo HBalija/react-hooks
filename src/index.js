cc729504c6341889d8ebb05c8929219150c82e8e
HBalija <balija.hrvoje@gmail.com>
2020-04-12 17:28:29 +0200

    Add context API and useContext hook

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import AuthContextProvider from './context/auth-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const jsx = (
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
+
+ReactDOM.render(jsx, document.getElementById('root'));
